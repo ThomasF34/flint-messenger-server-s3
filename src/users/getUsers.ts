027fa0686526c98061258149d9407c8389dba65d
Olivier Bernard <OBernard2@slb.com>
2020-06-06 22:00:25 +0200

    user update date missing

--- src/users/getUsers.ts
+++ src/users/getUsers.ts
@@ -3,7 +3,7 @@ import { User } from './model';
 
 export async function getUsers(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    const users = await User.find({}, '_id lastName firstName status').lean();
+    const users = await User.find({}, '_id lastName firstName status updatedAt').lean();
     res.json(users);
   } catch (error) {
     next(error);
